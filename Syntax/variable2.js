a3b005de56a7b19f3405c9f80ca088abf3f18511
kho903 <gmldnr2222@naver.com>
2019-11-11 18:16:43 +0900

    변수 활용

---
+++ Syntax/variable2.js
@@ -0,0 +1,26 @@
+name = "KIM";
+("+name+");
+var letter =
+  "Esse " +
+  name +
+  " ea officia et est. Veniam amet id do irure. Ea esse mollit reprehenderit exercitation ex ullamco ea cillum dolore.Dolor pariatur dolor qui occaecat fugiat magna magna qui. Elit duis minim labore mollit amet. Anim nostrud elit duis esse magna proident deserunt commodo mollit consequat sit amet do quis. Nulla ut nulla pariatur ea qui sunt laboris nisi aliquip excepteur " +
+  name +
+  " elit " +
+  name +
+  ". " +
+  name +
+  " tempor commodo labore commodo in id ut. Exercitation culpa culpa mollit ut aute commodo dolore esse. Laboris fugiat aliquip fugiat est eu esse ad aute aliqua.Tempor est enim quis ullamco Lorem adipisicing dolor proident amet irure commodo irure exercitation adipisicing. Culpa eu ea mollit quis eiusmod voluptate voluptate. Aliquip sunt voluptate esse reprehenderit velit. Qui nulla deserunt non anim esse labore proident enim. Velit do enim non sint sunt elit reprehenderit amet cupidatat quis aliqua voluptate. Eu quis laboris deserunt ullamco proident. Tempor eiusmod laboris culpa irure magna laborum ut tempor voluptate incididunt " +
+  name +
+  " adipisicing " +
+  name +
+  " minim.Deserunt reprehenderit magna ullamco in labore. Lorem ad velit non sunt in ullamco est incididunt adipisicing " +
+  name +
+  " ex dolore anim " +
+  name +
+  ". Dolore sunt " +
+  name +
+  " occaecat cillum. Cillum id magna ut et ut occaecat nulla ex fugiat. Cillum id duis ex elit adipisicing in. Cillum nisi anim magna sunt voluptate. Ut non exercitation velit aliquip irure incididunt eu sint veniam veniam. Et elit tempor nulla eiusmod cupidatat proident culpa consequat ad ex aute. Tempor non labore exercitation ipsum velit velit laborum " +
+  name +
+  " dolore.";
+
+console.log(letter);
