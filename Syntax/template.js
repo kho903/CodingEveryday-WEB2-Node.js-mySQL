e1f8494f0049cab95fdedc365cedcc1baafde63e
kho903 <gmldnr2222@naver.com>
2019-11-11 18:28:38 +0900

    Tamplate Literal

---
+++ Syntax/template.js
@@ -0,0 +1,33 @@
+var name = "KIM";
+var letter =
+  "Dear " +
+  name +
+  "Aliquip officia " +
+  name +
+  " ea cillum enim fugiat aliquip " +
+  name +
+  " velit aute. Ipsum id " +
+  name +
+  " eu do dolore sint " +
+  name +
+  " elit enim magna ullamco.\n\n\
+   Mollit laborum eu anim occaecat. " +
+  name +
+  " est ex mollit ad. Veniam irure nostrud veniam cillum. Do " +
+  name +
+  " pariatur eiusmod esse Sint duis enim esse ea aliquip " +
+  name +
+  " \n\
+  reprehenderit in.\
+  Amet cillum fugiat mollit mollit elit " +
+  name +
+  " id sunt. Elit velit\
+   dolore sit ex." +
+  name +
+  " minim.";
+var letter = `Dear ${name}
+ officia nisi ea cillum enim fugiat aliquip qui velit aute. Ipsum id cupidatat eu do dolore sint ${name} elit enim magna ullamco. ${name}
+  laborum eu anim occaecat. Nisi est ex mollit ad. Veniam 
+ ${name} nostrud veniam cillum. Do incididunt pariatur eiusmod esse Sint ${name} enim esse ea aliquip ${name} reprehenderit in. Amet cillum fugiat mollit mollit elit ${name} id sunt. Elit velit dolore sit ex.${name}
+  minim.`;
+console.log(letter);
