5d5e571b02b216f6a303c26f2bf8fdcedc756354
kho903 <gmldnr2222@naver.com>
2019-11-12 23:32:19 +0900

    JavaScript - 함수의 출력

--- Syntax/function2.js
+++ Syntax/function2.js
@@ -2,7 +2,7 @@ console.log(Math.round(1.6)); //2
 console.log(Math.round(1.4)); //1 반올림 함수
 
 function sum(first, second) /*parameter*/ {
-  console.log(first + second);
+  return first + second; //return이 되면 그 즉시 함수는 종료된다.
 }
 
-sum(2, 4); // argument
+console.log(sum(2, 4)); // argument
