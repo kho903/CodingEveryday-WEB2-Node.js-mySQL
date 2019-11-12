35425605e142d50c5c6988901fa99c4ef5510499
kho903 <gmldnr2222@naver.com>
2019-11-12 23:20:19 +0900

    JavaScript - 함수의 생성

---
+++ Syntax/function.js
@@ -0,0 +1,23 @@
+f123();
+console.log("a");
+console.log("e");
+console.log("w");
+f123();
+console.log("k");
+console.log("f");
+f123();
+console.log("f");
+console.log("b");
+f123();
+console.log("t");
+console.log("z");
+console.log("w");
+console.log("p");
+f123();
+
+function f123() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
