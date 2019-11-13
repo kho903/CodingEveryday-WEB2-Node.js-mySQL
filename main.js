6bc3cd5b830fda4384731d2394b1fcf5cf1344ca
kho903 <gmldnr2222@naver.com>
2019-11-13 22:40:15 +0900

    웹앱-글생성 - UI 만들기

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function templateHTML(title, list, body) {
     <body>
       <h1><a href="/">WEB</a></h1>
       ${list}
+      <a href="/create">create</a>
       ${body}
     </body>
   </html>
@@ -32,7 +33,6 @@ var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-
   if (pathname === "/") {
     if (queryData.id == undefined) {
       fs.readdir("./data", function(error, filelist) {
@@ -65,6 +65,28 @@ var app = http.createServer(function(request, response) {
         });
       });
     }
+  } else if (pathname === "/create") {
+    fs.readdir("./data", function(error, filelist) {
+      var title = "WEB - create";
+      var list = templateList(filelist);
+      var template = templateHTML(
+        title,
+        list,
+        `
+        <form action="http://localhost:4000/process_create" method="post">
+        <p><input type="text" name="title" placeholder="title" /></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit" />
+        </p>
+      </form>
+      `
+      );
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end("Not Found");
