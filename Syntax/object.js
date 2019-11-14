f666705807f3736953718ad8a88240429542779f
kho903 <gmldnr2222@naver.com>
2019-11-14 17:21:56 +0900

    JavaScript - 객체 - 형식(반복문을 이용한 출력)

--- Syntax/object.js
+++ Syntax/object.js
@@ -1,5 +1,14 @@
 var members = ["KIM", "LEE", "PARK"];
 console.log(members[1]); //LEE
+var i = 0;
+while (i < members.length) {
+  console.log("array loop", members[i]);
+  i += 1;
+}
 
 var roles = { programmer: "KIM", designer: "LEE", manager: "PARK" };
-console.log(roles.designer); //LEE
+console.log(roles["designer"]); //LEE
+
+for (var name in roles) {
+  console.log("object = >", name, "value = >", roles[name]);
+}
