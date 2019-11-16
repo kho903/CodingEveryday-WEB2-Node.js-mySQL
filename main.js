6905f6fa329a3e37146bee2066ac831df91b90bb
kho903 <gmldnr2222@naver.com>
2019-11-16 15:19:03 +0900

    웹앱-npm을 통해서 부품으로 사용할 모듈 사용하기, 출력정보에 대한 보안

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var qs = require("querystring");
 
 var template = require("./lib/template.js");
 var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
@@ -31,15 +32,19 @@ var app = http.createServer(function(request, response) {
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
           var title = queryData.id;
+          var sanitizedTitle = sanitizeHtml(title);
+          var sanitizedDescription = sanitizeHtml(description, {
+            allowedTags: ["h1"]
+          });
           var list = template.list(filelist);
           var html = template.HTML(
-            title,
+            sanitizedTitle,
             list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
             `<a href="/create">create</a> 
-            <a href="/update?id=${title}">update</a>
+            <a href="/update?id=${sanitizedTitle}">update</a>
             <form action="delete_process" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
             </form>
             `
